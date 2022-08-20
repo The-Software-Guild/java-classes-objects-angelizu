98c0c78917373a1ef381fcf0f817d5102c6b07fd
angel.izu <angelizux@yahoo.com>
2022-08-20 19:22:57 +0100

    0003: making changes and added file

--- src/com/sg/classroster/App.java
+++ src/com/sg/classroster/App.java
@@ -2,6 +2,6 @@ package com.sg.classroster;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello test");
+        System.out.println("Hello test again");
     }
 }
