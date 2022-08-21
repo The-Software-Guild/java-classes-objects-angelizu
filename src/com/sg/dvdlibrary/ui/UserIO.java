d845144b762a7ced7307c33451223a74826e3484
angel.izu <angelizux@yahoo.com>
2022-08-21 21:28:46 +0100

    0003: added code to UserIO

--- src/com/sg/dvdlibrary/ui/UserIO.java
+++ src/com/sg/dvdlibrary/ui/UserIO.java
@@ -1,4 +1,25 @@
 package com.sg.dvdlibrary.ui;
 
 public interface UserIO {
+
+    String readString(String prompt);
+
+    void print(String msg);
+
+    int readInt(String prompt);
+
+    int readInt(String prompt, int min, int max);
+
+    float readFloat(String prompt);
+
+    float readFloat(String prompt, float min, float max);
+
+    double readDouble(String prompt);
+
+    double readDouble(String prompt, double min, double max);
+
+    long readLong(String prompt);
+
+    long readLong(String prompt, long min, long max);
+
 }
