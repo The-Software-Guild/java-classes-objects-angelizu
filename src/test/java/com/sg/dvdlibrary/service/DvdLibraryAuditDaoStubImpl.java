da97ee78e95046f536451f558725f4392eea7f5b
angel.izu <angelizux@yahoo.com>
2022-08-30 15:34:08 +0100

    0023: added stub file

---
+++ src/test/java/com/sg/dvdlibrary/service/DvdLibraryAuditDaoStubImpl.java
@@ -0,0 +1,11 @@
+package com.sg.dvdlibrary.service;
+
+import com.sg.dvdlibrary.dao.DvdLibraryAuditDao;
+import com.sg.dvdlibrary.dao.DvdLibraryPersistenceException;
+
+public class DvdLibraryAuditDaoStubImpl implements DvdLibraryAuditDao {
+    @Override
+    public void writeAuditEntry(String entry) throws DvdLibraryPersistenceException {
+
+    }
+}
