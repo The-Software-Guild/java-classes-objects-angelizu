9423845f2fe0328b1a3e83ca1ad99948ab4a02da
angel.izu <angelizux@yahoo.com>
2022-08-30 16:33:11 +0100

    0025: added service layer test

--- src/main/java/com/sg/dvdlibrary/service/DvdLibraryServiceLayerImplTest.java
+++ src/main/java/com/sg/dvdlibrary/service/DvdLibraryServiceLayerImplTest.java
@@ -53,6 +53,106 @@ class DvdLibraryServiceLayerImplTest {
             //ASSERT
             fail("Dvd was valid. No exception should have been thrown");
         }
-
     }
+
+    @Test
+    public void testCreateDuplicateIdDvd() {
+        //ARRANGE
+        Dvd dvd = new Dvd("0001");
+        dvd.setTitle("Iron Man");
+        dvd.setReleaseDate("02/05/2008");
+        dvd.setMpaaRating("PG-13");
+        dvd.setDirectorName("Jon Favreau");
+        dvd.setStudio("Marvel");
+        dvd.setUserRating("I enjoyed it");
+
+        try {
+            service.createDvd(dvd);
+            fail("Expected Duplicate Exception was not thrown.");
+        } catch (DvdLibraryDataValidationException | DvdLibraryPersistenceException e) {
+            fail("Incorrect exception was thrown.");
+        } catch (DvdLibraryDuplicateIdException e) {
+            return;
+        }
+    }
+
+    @Test
+    public void testCreateDvdInvalidData() throws Exception {
+        //ARRANGE
+        Dvd dvd = new Dvd("0002");
+        dvd.setTitle("");
+        dvd.setReleaseDate("22/07/2009");
+        dvd.setMpaaRating("PG-13");
+        dvd.setDirectorName("Anne Fletcher");
+        dvd.setStudio("Walt Disney");
+        dvd.setUserRating("Favourite romance movie!");
+
+        //ACT
+        try {
+            service.createDvd(dvd);
+            fail("Expected ValidationException was not thrown;");
+        } catch (DvdLibraryDuplicateIdException | DvdLibraryPersistenceException e) {
+            //ASSERT
+            fail("Incorrect exception was thrown.");
+        } catch (DvdLibraryDataValidationException e) {
+            return;
+        }
+    }
+
+    @Test
+    public void testGetAllDvds() throws Exception {
+        //ARRANGE
+        Dvd testClone = new Dvd("0001");
+        testClone.setTitle("Iron Man");
+        testClone.setReleaseDate("02/05/2008");
+        testClone.setMpaaRating("PG-13");
+        testClone.setDirectorName("Jon Favreau");
+        testClone.setStudio("Marvel");
+        testClone.setUserRating("I enjoyed it");
+
+        //ACT & ASSERT
+        assertEquals(1, service.getAllDvds().size(), "Should only have one dvd");
+        assertTrue(service.getAllDvds().contains(testClone));
+    }
+
+    @Test
+    public void testGetDvd() throws Exception {
+        //ARRANGE
+        Dvd testClone = new Dvd("0001");
+        testClone.setTitle("Iron Man");
+        testClone.setReleaseDate("02/05/2008");
+        testClone.setMpaaRating("PG-13");
+        testClone.setDirectorName("Jon Favreau");
+        testClone.setStudio("Marvel");
+        testClone.setUserRating("I enjoyed it");
+
+        //ACT & ASSERT
+        Dvd shouldBeIron = service.getDvd("0001");
+        assertNotNull(shouldBeIron, "Getting 0001 should be not null");
+        assertEquals(testClone, shouldBeIron, "Dvd stored under 0001 should be Iron Man");
+
+        Dvd shouldBeNull = service.getDvd("0042");
+        assertNull(shouldBeNull, "Getting 0042 should be null");
+    }
+
+    @Test
+    public void testRemoveDvd() throws Exception {
+        //ARRANGE
+        Dvd testClone = new Dvd("0001");
+        testClone.setTitle("Iron Man");
+        testClone.setReleaseDate("02/05/2008");
+        testClone.setMpaaRating("PG-13");
+        testClone.setDirectorName("Jon Favreau");
+        testClone.setStudio("Marvel");
+        testClone.setUserRating("I enjoyed it");
+
+        //ACT & ASSERT
+        Dvd shouldBeIron = service.removeDvd("0001");
+        assertNotNull(shouldBeIron, "Getting 0001 should be not null");
+        assertEquals(testClone, shouldBeIron, "Dvd stored under 0001 should be Iron Man");
+
+        Dvd shouldBeNull = service.removeDvd("0042");
+        assertNull(shouldBeNull, "Getting 0042 should be null");
+    }
+
 }
